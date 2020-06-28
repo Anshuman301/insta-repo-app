81a1909f7bbe721c1fb31edcf7e3f98571ca96a5
Anshuman301 <anshumanprsd@gmail.com>
2020-06-29 01:21:01 +0530

    Updates

---
+++ precache-manifest.a64e053445c70ed20a132ebc88123c18.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c1515bb798826fa81a195c4d32f1e5",
+    "url": "/insta-repo-app/index.html"
+  },
+  {
+    "revision": "95a2d4cc9ce70555189e",
+    "url": "/insta-repo-app/static/css/main.95c423ca.chunk.css"
+  },
+  {
+    "revision": "2a258b611c08c7f74606",
+    "url": "/insta-repo-app/static/js/2.5993779a.chunk.js"
+  },
+  {
+    "revision": "95a2d4cc9ce70555189e",
+    "url": "/insta-repo-app/static/js/main.a1afb31a.chunk.js"
+  },
+  {
+    "revision": "f49d3bb629b4e2905ea2",
+    "url": "/insta-repo-app/static/js/runtime~main.d5145bdd.js"
+  },
+  {
+    "revision": "902c4365b6e050acc85eab3d8c238d94",
+    "url": "/insta-repo-app/static/media/explore.902c4365.svg"
+  },
+  {
+    "revision": "0ed5fbdb544618b2919b98c91fe23108",
+    "url": "/insta-repo-app/static/media/home.0ed5fbdb.svg"
+  },
+  {
+    "revision": "bb026c4d996a5d563f7fceb5166175e1",
+    "url": "/insta-repo-app/static/media/instagram.bb026c4d.svg"
+  },
+  {
+    "revision": "c05f2fa37a7d7705106322c6fa1aab54",
+    "url": "/insta-repo-app/static/media/user.c05f2fa3.svg"
+  }
+]);
