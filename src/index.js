20d1814ed0505bfb142fb0c1322d6f507e9c46d4
Anshuman301 <anshumanprsd@gmail.com>
2020-06-29 01:24:33 +0530

    done

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-<BrowserRouter>
+<BrowserRouter basename='/insta-repo-app'>
 <App/>
 </BrowserRouter>, document.getElementById('root'));
 
